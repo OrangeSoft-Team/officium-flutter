94e0596cd0eb0010710797a1867a0d4a0c5d8282
Andrés De Armas <andresgdearmas@gmail.com>
2021-06-24 17:32:01 -0400

    Se crea la carpeta "core" del proyecto
    La carpeta core contiene 
    - Las clase abstracta Value object
    - La factoría de Value Failures, fallos que tienen significado en el dominio,  (Contiene los distintos fallos que puede manejar un Value Object al momento de la validación)
    - Errores: Contiene los errores propios del sistema

---
+++ lib/dominio/core/errors.dart
@@ -0,0 +1,16 @@
+import 'package:officium_flutter/dominio/core/factory_failures.dart';
+
+//Aquí estarán todas las clases de Error
+
+class UnexpectedValueError extends Error {
+  final ValueFailure valueFailure;
+
+  UnexpectedValueError(this.valueFailure);
+
+  @override
+  String toString() {
+    const explanation =
+        'Encountered a ValueFailure at an unrecoverable point. Terminating.';
+    return Error.safeToString('$explanation Failure was: $valueFailure');
+  }
+}
---
+++ lib/dominio/core/value_object.dart
@@ -0,0 +1,59 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:officium_flutter/dominio/core/errors.dart';
+import 'package:officium_flutter/dominio/core/factory_failures.dart';
+import 'package:uuid/uuid.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    // id = identity - same as writing (right) => right
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
+
+  Either<ValueFailure<dynamic>, Unit> get failureOrUnit {
+    return value.fold(
+      (l) => left(l),
+      (r) => right(unit),
+    );
+  }
+
+  bool isValid() => value.isRight();
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is ValueObject<T> && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+}
+
+class UniqueId extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory UniqueId() {
+    return UniqueId._(
+      right(const Uuid().v1()),
+    );
+  }
+
+  factory UniqueId.fromUniqueString(String uniqueId) {
+    return UniqueId._(
+      right(uniqueId),
+    );
+  }
+
+  const UniqueId._(this.value);
+}
