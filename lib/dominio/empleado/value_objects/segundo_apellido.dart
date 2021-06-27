049d049667121a9d6a892aeeb52429f49745692c
Leonardo Ruiz <laruiz.18@est.ucab.edu.ve>
2021-06-27 16:31:19 -0400

    Corrige imports

--- lib/dominio/empleado/value_objects/segundo_apellido.dart
+++ lib/dominio/empleado/value_objects/segundo_apellido.dart
@@ -1,6 +1,6 @@
-import 'package:officium_flutter/dominio/comun/excepciones_dominio/valores_errones_value_object/factoriaValorErroneo.dart';
 import 'package:dartz/dartz.dart';
-import 'package:officium_flutter/dominio/comun/servicios_dominio/validadores_value_objects/validador_longitudString.dart';
+import 'package:officium_flutter/dominio/comun/excepciones_dominio/valores_errones_value_object/factoria_valor_erroneo.dart';
+import 'package:officium_flutter/dominio/comun/servicios_dominio/validadores_value_objects/validador_longitud_string.dart';
 import 'package:officium_flutter/dominio/comun/value_object.dart';
 
 class SegundoApellido extends ValueObject<String> {
