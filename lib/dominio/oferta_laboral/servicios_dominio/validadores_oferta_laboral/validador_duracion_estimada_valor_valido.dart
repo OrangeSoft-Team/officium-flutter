fcd77390d362d07bb42df72e73c98aaf7de14ffc
Leonardo Ruiz <laruiz.18@est.ucab.edu.ve>
2021-06-28 17:14:48 -0400

    Elimina imports innecesarios y añade semantica a diferentes funciones

--- lib/dominio/oferta_laboral/servicios_dominio/validadores_oferta_laboral/validador_duracion_estimada_valor_valido.dart
+++ lib/dominio/oferta_laboral/servicios_dominio/validadores_oferta_laboral/validador_duracion_estimada_valor_valido.dart
@@ -1,6 +1,3 @@
-import 'package:dartz/dartz.dart';
-import 'package:officium_flutter/dominio/comun/excepciones_dominio/valores_errones_value_object/factoria_valor_erroneo.dart';
-
 bool validadorDuracionEstimadaValor(
     int valor, int minNumeroValor, int maxNumeroValor) {
   if (valor >= minNumeroValor && valor <= maxNumeroValor) {
