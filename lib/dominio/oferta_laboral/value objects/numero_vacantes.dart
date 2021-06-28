997c845733d1a83ffd30e80e2dc8c93fd7e9df64
Leonardo Ruiz <laruiz.18@est.ucab.edu.ve>
2021-06-28 16:33:07 -0400

    Resuelve imports

--- lib/dominio/oferta_laboral/value objects/numero_vacantes.dart
+++ lib/dominio/oferta_laboral/value objects/numero_vacantes.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart' show Either, right;
 import 'package:officium_flutter/dominio/comun/excepciones_dominio/valores_errones_value_object/factoria_valor_erroneo.dart';
 import 'package:officium_flutter/dominio/comun/value_object.dart';
-import 'package:officium_flutter/dominio/oferta%20laboral/servicios_dominio/validadores_oferta_laboral/validador_vacantes.dart';
+import 'package:officium_flutter/dominio/oferta_laboral/servicios_dominio/validadores_oferta_laboral/validador_vacantes.dart';
 
 class NumeroVacantes extends ValueObject<int> {
   @override
