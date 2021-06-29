f5f6bb1c9c5b752aab3819816781aa7c55946d7a
Andres De Armas <andresgdearmas@gmail.com>
2021-06-29 15:19:09 -0400

    Crea Facha de Autentificacion
    - Se crea el servicio de dominio de la facha de autentificacion, solo se crea el contrato. 
    - Crea value objects para Email, Password y Estado asi como tambien sus validadores.

--- lib/dominio/autentificacion/vaue_objecs/email.dart
+++ lib/dominio/autentificacion/vaue_objecs/email.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
+import 'package:officium_flutter/dominio/autentificacion/servicios_dominio/validadores/validador_email.dart';
 import 'package:officium_flutter/dominio/comun/excepciones_dominio/valores_errones_value_object/factoria_valor_erroneo.dart';
-import 'package:officium_flutter/dominio/comun/servicios_dominio/validadores_value_objects/validador_email.dart';
 import 'package:officium_flutter/dominio/comun/value_object.dart';
 
 class EmailAddress extends ValueObject<String> {
