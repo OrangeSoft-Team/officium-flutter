e904d6a8e08f3f7550d14fa630ad346b26166a37
unknown <sampleacc620@gmail.com>
2021-07-01 20:46:31 +0200

    Generacion DTOS para oferta laboral Se generaron los dtos para oferta laboral, oferta laboral detalle y postulacion oferta laboral.

---
+++ lib/infraestructura/oferta_laboral/modelos/oferta_laboral_detalle_dto.freezed.dart
@@ -0,0 +1,493 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'oferta_laboral_detalle_dto.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+OfertaLaboralDetalleDTO _$OfertaLaboralDetalleDTOFromJson(
+    Map<String, dynamic> json) {
+  return _OfertaLaboralDetalleDTO.fromJson(json);
+}
+
+/// @nodoc
+class _$OfertaLaboralDetalleDTOTearOff {
+  const _$OfertaLaboralDetalleDTOTearOff();
+
+  _OfertaLaboralDetalleDTO call(
+      {required String uuid,
+      required String titulo,
+      required String fechaPublicacion,
+      required String fechaModificacion,
+      required String cargo,
+      required double sueldo,
+      required String descripcion,
+      required int duracionEstimadaValor,
+      required String duracionEstimadaEscala,
+      required String turnoTrabajo,
+      required int numeroVacantes,
+      required String uuidEmpresa,
+      required String empresaNombre,
+      required String direccionEmpresa}) {
+    return _OfertaLaboralDetalleDTO(
+      uuid: uuid,
+      titulo: titulo,
+      fechaPublicacion: fechaPublicacion,
+      fechaModificacion: fechaModificacion,
+      cargo: cargo,
+      sueldo: sueldo,
+      descripcion: descripcion,
+      duracionEstimadaValor: duracionEstimadaValor,
+      duracionEstimadaEscala: duracionEstimadaEscala,
+      turnoTrabajo: turnoTrabajo,
+      numeroVacantes: numeroVacantes,
+      uuidEmpresa: uuidEmpresa,
+      empresaNombre: empresaNombre,
+      direccionEmpresa: direccionEmpresa,
+    );
+  }
+
+  OfertaLaboralDetalleDTO fromJson(Map<String, Object> json) {
+    return OfertaLaboralDetalleDTO.fromJson(json);
+  }
+}
+
+/// @nodoc
+const $OfertaLaboralDetalleDTO = _$OfertaLaboralDetalleDTOTearOff();
+
+/// @nodoc
+mixin _$OfertaLaboralDetalleDTO {
+  String get uuid => throw _privateConstructorUsedError;
+  String get titulo => throw _privateConstructorUsedError;
+  String get fechaPublicacion => throw _privateConstructorUsedError;
+  String get fechaModificacion => throw _privateConstructorUsedError;
+  String get cargo => throw _privateConstructorUsedError;
+  double get sueldo => throw _privateConstructorUsedError;
+  String get descripcion => throw _privateConstructorUsedError;
+  int get duracionEstimadaValor => throw _privateConstructorUsedError;
+  String get duracionEstimadaEscala => throw _privateConstructorUsedError;
+  String get turnoTrabajo => throw _privateConstructorUsedError;
+  int get numeroVacantes => throw _privateConstructorUsedError;
+  String get uuidEmpresa => throw _privateConstructorUsedError;
+  String get empresaNombre => throw _privateConstructorUsedError;
+  String get direccionEmpresa => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $OfertaLaboralDetalleDTOCopyWith<OfertaLaboralDetalleDTO> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $OfertaLaboralDetalleDTOCopyWith<$Res> {
+  factory $OfertaLaboralDetalleDTOCopyWith(OfertaLaboralDetalleDTO value,
+          $Res Function(OfertaLaboralDetalleDTO) then) =
+      _$OfertaLaboralDetalleDTOCopyWithImpl<$Res>;
+  $Res call(
+      {String uuid,
+      String titulo,
+      String fechaPublicacion,
+      String fechaModificacion,
+      String cargo,
+      double sueldo,
+      String descripcion,
+      int duracionEstimadaValor,
+      String duracionEstimadaEscala,
+      String turnoTrabajo,
+      int numeroVacantes,
+      String uuidEmpresa,
+      String empresaNombre,
+      String direccionEmpresa});
+}
+
+/// @nodoc
+class _$OfertaLaboralDetalleDTOCopyWithImpl<$Res>
+    implements $OfertaLaboralDetalleDTOCopyWith<$Res> {
+  _$OfertaLaboralDetalleDTOCopyWithImpl(this._value, this._then);
+
+  final OfertaLaboralDetalleDTO _value;
+  // ignore: unused_field
+  final $Res Function(OfertaLaboralDetalleDTO) _then;
+
+  @override
+  $Res call({
+    Object? uuid = freezed,
+    Object? titulo = freezed,
+    Object? fechaPublicacion = freezed,
+    Object? fechaModificacion = freezed,
+    Object? cargo = freezed,
+    Object? sueldo = freezed,
+    Object? descripcion = freezed,
+    Object? duracionEstimadaValor = freezed,
+    Object? duracionEstimadaEscala = freezed,
+    Object? turnoTrabajo = freezed,
+    Object? numeroVacantes = freezed,
+    Object? uuidEmpresa = freezed,
+    Object? empresaNombre = freezed,
+    Object? direccionEmpresa = freezed,
+  }) {
+    return _then(_value.copyWith(
+      uuid: uuid == freezed
+          ? _value.uuid
+          : uuid // ignore: cast_nullable_to_non_nullable
+              as String,
+      titulo: titulo == freezed
+          ? _value.titulo
+          : titulo // ignore: cast_nullable_to_non_nullable
+              as String,
+      fechaPublicacion: fechaPublicacion == freezed
+          ? _value.fechaPublicacion
+          : fechaPublicacion // ignore: cast_nullable_to_non_nullable
+              as String,
+      fechaModificacion: fechaModificacion == freezed
+          ? _value.fechaModificacion
+          : fechaModificacion // ignore: cast_nullable_to_non_nullable
+              as String,
+      cargo: cargo == freezed
+          ? _value.cargo
+          : cargo // ignore: cast_nullable_to_non_nullable
+              as String,
+      sueldo: sueldo == freezed
+          ? _value.sueldo
+          : sueldo // ignore: cast_nullable_to_non_nullable
+              as double,
+      descripcion: descripcion == freezed
+          ? _value.descripcion
+          : descripcion // ignore: cast_nullable_to_non_nullable
+              as String,
+      duracionEstimadaValor: duracionEstimadaValor == freezed
+          ? _value.duracionEstimadaValor
+          : duracionEstimadaValor // ignore: cast_nullable_to_non_nullable
+              as int,
+      duracionEstimadaEscala: duracionEstimadaEscala == freezed
+          ? _value.duracionEstimadaEscala
+          : duracionEstimadaEscala // ignore: cast_nullable_to_non_nullable
+              as String,
+      turnoTrabajo: turnoTrabajo == freezed
+          ? _value.turnoTrabajo
+          : turnoTrabajo // ignore: cast_nullable_to_non_nullable
+              as String,
+      numeroVacantes: numeroVacantes == freezed
+          ? _value.numeroVacantes
+          : numeroVacantes // ignore: cast_nullable_to_non_nullable
+              as int,
+      uuidEmpresa: uuidEmpresa == freezed
+          ? _value.uuidEmpresa
+          : uuidEmpresa // ignore: cast_nullable_to_non_nullable
+              as String,
+      empresaNombre: empresaNombre == freezed
+          ? _value.empresaNombre
+          : empresaNombre // ignore: cast_nullable_to_non_nullable
+              as String,
+      direccionEmpresa: direccionEmpresa == freezed
+          ? _value.direccionEmpresa
+          : direccionEmpresa // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$OfertaLaboralDetalleDTOCopyWith<$Res>
+    implements $OfertaLaboralDetalleDTOCopyWith<$Res> {
+  factory _$OfertaLaboralDetalleDTOCopyWith(_OfertaLaboralDetalleDTO value,
+          $Res Function(_OfertaLaboralDetalleDTO) then) =
+      __$OfertaLaboralDetalleDTOCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String uuid,
+      String titulo,
+      String fechaPublicacion,
+      String fechaModificacion,
+      String cargo,
+      double sueldo,
+      String descripcion,
+      int duracionEstimadaValor,
+      String duracionEstimadaEscala,
+      String turnoTrabajo,
+      int numeroVacantes,
+      String uuidEmpresa,
+      String empresaNombre,
+      String direccionEmpresa});
+}
+
+/// @nodoc
+class __$OfertaLaboralDetalleDTOCopyWithImpl<$Res>
+    extends _$OfertaLaboralDetalleDTOCopyWithImpl<$Res>
+    implements _$OfertaLaboralDetalleDTOCopyWith<$Res> {
+  __$OfertaLaboralDetalleDTOCopyWithImpl(_OfertaLaboralDetalleDTO _value,
+      $Res Function(_OfertaLaboralDetalleDTO) _then)
+      : super(_value, (v) => _then(v as _OfertaLaboralDetalleDTO));
+
+  @override
+  _OfertaLaboralDetalleDTO get _value =>
+      super._value as _OfertaLaboralDetalleDTO;
+
+  @override
+  $Res call({
+    Object? uuid = freezed,
+    Object? titulo = freezed,
+    Object? fechaPublicacion = freezed,
+    Object? fechaModificacion = freezed,
+    Object? cargo = freezed,
+    Object? sueldo = freezed,
+    Object? descripcion = freezed,
+    Object? duracionEstimadaValor = freezed,
+    Object? duracionEstimadaEscala = freezed,
+    Object? turnoTrabajo = freezed,
+    Object? numeroVacantes = freezed,
+    Object? uuidEmpresa = freezed,
+    Object? empresaNombre = freezed,
+    Object? direccionEmpresa = freezed,
+  }) {
+    return _then(_OfertaLaboralDetalleDTO(
+      uuid: uuid == freezed
+          ? _value.uuid
+          : uuid // ignore: cast_nullable_to_non_nullable
+              as String,
+      titulo: titulo == freezed
+          ? _value.titulo
+          : titulo // ignore: cast_nullable_to_non_nullable
+              as String,
+      fechaPublicacion: fechaPublicacion == freezed
+          ? _value.fechaPublicacion
+          : fechaPublicacion // ignore: cast_nullable_to_non_nullable
+              as String,
+      fechaModificacion: fechaModificacion == freezed
+          ? _value.fechaModificacion
+          : fechaModificacion // ignore: cast_nullable_to_non_nullable
+              as String,
+      cargo: cargo == freezed
+          ? _value.cargo
+          : cargo // ignore: cast_nullable_to_non_nullable
+              as String,
+      sueldo: sueldo == freezed
+          ? _value.sueldo
+          : sueldo // ignore: cast_nullable_to_non_nullable
+              as double,
+      descripcion: descripcion == freezed
+          ? _value.descripcion
+          : descripcion // ignore: cast_nullable_to_non_nullable
+              as String,
+      duracionEstimadaValor: duracionEstimadaValor == freezed
+          ? _value.duracionEstimadaValor
+          : duracionEstimadaValor // ignore: cast_nullable_to_non_nullable
+              as int,
+      duracionEstimadaEscala: duracionEstimadaEscala == freezed
+          ? _value.duracionEstimadaEscala
+          : duracionEstimadaEscala // ignore: cast_nullable_to_non_nullable
+              as String,
+      turnoTrabajo: turnoTrabajo == freezed
+          ? _value.turnoTrabajo
+          : turnoTrabajo // ignore: cast_nullable_to_non_nullable
+              as String,
+      numeroVacantes: numeroVacantes == freezed
+          ? _value.numeroVacantes
+          : numeroVacantes // ignore: cast_nullable_to_non_nullable
+              as int,
+      uuidEmpresa: uuidEmpresa == freezed
+          ? _value.uuidEmpresa
+          : uuidEmpresa // ignore: cast_nullable_to_non_nullable
+              as String,
+      empresaNombre: empresaNombre == freezed
+          ? _value.empresaNombre
+          : empresaNombre // ignore: cast_nullable_to_non_nullable
+              as String,
+      direccionEmpresa: direccionEmpresa == freezed
+          ? _value.direccionEmpresa
+          : direccionEmpresa // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_OfertaLaboralDetalleDTO implements _OfertaLaboralDetalleDTO {
+  const _$_OfertaLaboralDetalleDTO(
+      {required this.uuid,
+      required this.titulo,
+      required this.fechaPublicacion,
+      required this.fechaModificacion,
+      required this.cargo,
+      required this.sueldo,
+      required this.descripcion,
+      required this.duracionEstimadaValor,
+      required this.duracionEstimadaEscala,
+      required this.turnoTrabajo,
+      required this.numeroVacantes,
+      required this.uuidEmpresa,
+      required this.empresaNombre,
+      required this.direccionEmpresa});
+
+  factory _$_OfertaLaboralDetalleDTO.fromJson(Map<String, dynamic> json) =>
+      _$_$_OfertaLaboralDetalleDTOFromJson(json);
+
+  @override
+  final String uuid;
+  @override
+  final String titulo;
+  @override
+  final String fechaPublicacion;
+  @override
+  final String fechaModificacion;
+  @override
+  final String cargo;
+  @override
+  final double sueldo;
+  @override
+  final String descripcion;
+  @override
+  final int duracionEstimadaValor;
+  @override
+  final String duracionEstimadaEscala;
+  @override
+  final String turnoTrabajo;
+  @override
+  final int numeroVacantes;
+  @override
+  final String uuidEmpresa;
+  @override
+  final String empresaNombre;
+  @override
+  final String direccionEmpresa;
+
+  @override
+  String toString() {
+    return 'OfertaLaboralDetalleDTO(uuid: $uuid, titulo: $titulo, fechaPublicacion: $fechaPublicacion, fechaModificacion: $fechaModificacion, cargo: $cargo, sueldo: $sueldo, descripcion: $descripcion, duracionEstimadaValor: $duracionEstimadaValor, duracionEstimadaEscala: $duracionEstimadaEscala, turnoTrabajo: $turnoTrabajo, numeroVacantes: $numeroVacantes, uuidEmpresa: $uuidEmpresa, empresaNombre: $empresaNombre, direccionEmpresa: $direccionEmpresa)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _OfertaLaboralDetalleDTO &&
+            (identical(other.uuid, uuid) ||
+                const DeepCollectionEquality().equals(other.uuid, uuid)) &&
+            (identical(other.titulo, titulo) ||
+                const DeepCollectionEquality().equals(other.titulo, titulo)) &&
+            (identical(other.fechaPublicacion, fechaPublicacion) ||
+                const DeepCollectionEquality()
+                    .equals(other.fechaPublicacion, fechaPublicacion)) &&
+            (identical(other.fechaModificacion, fechaModificacion) ||
+                const DeepCollectionEquality()
+                    .equals(other.fechaModificacion, fechaModificacion)) &&
+            (identical(other.cargo, cargo) ||
+                const DeepCollectionEquality().equals(other.cargo, cargo)) &&
+            (identical(other.sueldo, sueldo) ||
+                const DeepCollectionEquality().equals(other.sueldo, sueldo)) &&
+            (identical(other.descripcion, descripcion) ||
+                const DeepCollectionEquality()
+                    .equals(other.descripcion, descripcion)) &&
+            (identical(other.duracionEstimadaValor, duracionEstimadaValor) ||
+                const DeepCollectionEquality().equals(
+                    other.duracionEstimadaValor, duracionEstimadaValor)) &&
+            (identical(other.duracionEstimadaEscala, duracionEstimadaEscala) ||
+                const DeepCollectionEquality().equals(
+                    other.duracionEstimadaEscala, duracionEstimadaEscala)) &&
+            (identical(other.turnoTrabajo, turnoTrabajo) ||
+                const DeepCollectionEquality()
+                    .equals(other.turnoTrabajo, turnoTrabajo)) &&
+            (identical(other.numeroVacantes, numeroVacantes) ||
+                const DeepCollectionEquality()
+                    .equals(other.numeroVacantes, numeroVacantes)) &&
+            (identical(other.uuidEmpresa, uuidEmpresa) ||
+                const DeepCollectionEquality()
+                    .equals(other.uuidEmpresa, uuidEmpresa)) &&
+            (identical(other.empresaNombre, empresaNombre) ||
+                const DeepCollectionEquality()
+                    .equals(other.empresaNombre, empresaNombre)) &&
+            (identical(other.direccionEmpresa, direccionEmpresa) ||
+                const DeepCollectionEquality()
+                    .equals(other.direccionEmpresa, direccionEmpresa)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(uuid) ^
+      const DeepCollectionEquality().hash(titulo) ^
+      const DeepCollectionEquality().hash(fechaPublicacion) ^
+      const DeepCollectionEquality().hash(fechaModificacion) ^
+      const DeepCollectionEquality().hash(cargo) ^
+      const DeepCollectionEquality().hash(sueldo) ^
+      const DeepCollectionEquality().hash(descripcion) ^
+      const DeepCollectionEquality().hash(duracionEstimadaValor) ^
+      const DeepCollectionEquality().hash(duracionEstimadaEscala) ^
+      const DeepCollectionEquality().hash(turnoTrabajo) ^
+      const DeepCollectionEquality().hash(numeroVacantes) ^
+      const DeepCollectionEquality().hash(uuidEmpresa) ^
+      const DeepCollectionEquality().hash(empresaNombre) ^
+      const DeepCollectionEquality().hash(direccionEmpresa);
+
+  @JsonKey(ignore: true)
+  @override
+  _$OfertaLaboralDetalleDTOCopyWith<_OfertaLaboralDetalleDTO> get copyWith =>
+      __$OfertaLaboralDetalleDTOCopyWithImpl<_OfertaLaboralDetalleDTO>(
+          this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$_$_OfertaLaboralDetalleDTOToJson(this);
+  }
+}
+
+abstract class _OfertaLaboralDetalleDTO implements OfertaLaboralDetalleDTO {
+  const factory _OfertaLaboralDetalleDTO(
+      {required String uuid,
+      required String titulo,
+      required String fechaPublicacion,
+      required String fechaModificacion,
+      required String cargo,
+      required double sueldo,
+      required String descripcion,
+      required int duracionEstimadaValor,
+      required String duracionEstimadaEscala,
+      required String turnoTrabajo,
+      required int numeroVacantes,
+      required String uuidEmpresa,
+      required String empresaNombre,
+      required String direccionEmpresa}) = _$_OfertaLaboralDetalleDTO;
+
+  factory _OfertaLaboralDetalleDTO.fromJson(Map<String, dynamic> json) =
+      _$_OfertaLaboralDetalleDTO.fromJson;
+
+  @override
+  String get uuid => throw _privateConstructorUsedError;
+  @override
+  String get titulo => throw _privateConstructorUsedError;
+  @override
+  String get fechaPublicacion => throw _privateConstructorUsedError;
+  @override
+  String get fechaModificacion => throw _privateConstructorUsedError;
+  @override
+  String get cargo => throw _privateConstructorUsedError;
+  @override
+  double get sueldo => throw _privateConstructorUsedError;
+  @override
+  String get descripcion => throw _privateConstructorUsedError;
+  @override
+  int get duracionEstimadaValor => throw _privateConstructorUsedError;
+  @override
+  String get duracionEstimadaEscala => throw _privateConstructorUsedError;
+  @override
+  String get turnoTrabajo => throw _privateConstructorUsedError;
+  @override
+  int get numeroVacantes => throw _privateConstructorUsedError;
+  @override
+  String get uuidEmpresa => throw _privateConstructorUsedError;
+  @override
+  String get empresaNombre => throw _privateConstructorUsedError;
+  @override
+  String get direccionEmpresa => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$OfertaLaboralDetalleDTOCopyWith<_OfertaLaboralDetalleDTO> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/infraestructura/oferta_laboral/modelos/postulacion_oferta_laboral_dto.freezed.dart
@@ -0,0 +1,218 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'postulacion_oferta_laboral_dto.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+PostulacionOfertaLaboralDTO _$PostulacionOfertaLaboralDTOFromJson(
+    Map<String, dynamic> json) {
+  return _PostulacionOfertaLaboralDTO.fromJson(json);
+}
+
+/// @nodoc
+class _$PostulacionOfertaLaboralDTOTearOff {
+  const _$PostulacionOfertaLaboralDTOTearOff();
+
+  _PostulacionOfertaLaboralDTO call(
+      {required String uuidEmpleado,
+      required String uuidEmpresa,
+      String? comentario}) {
+    return _PostulacionOfertaLaboralDTO(
+      uuidEmpleado: uuidEmpleado,
+      uuidEmpresa: uuidEmpresa,
+      comentario: comentario,
+    );
+  }
+
+  PostulacionOfertaLaboralDTO fromJson(Map<String, Object> json) {
+    return PostulacionOfertaLaboralDTO.fromJson(json);
+  }
+}
+
+/// @nodoc
+const $PostulacionOfertaLaboralDTO = _$PostulacionOfertaLaboralDTOTearOff();
+
+/// @nodoc
+mixin _$PostulacionOfertaLaboralDTO {
+  String get uuidEmpleado => throw _privateConstructorUsedError;
+  String get uuidEmpresa => throw _privateConstructorUsedError;
+  String? get comentario => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $PostulacionOfertaLaboralDTOCopyWith<PostulacionOfertaLaboralDTO>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PostulacionOfertaLaboralDTOCopyWith<$Res> {
+  factory $PostulacionOfertaLaboralDTOCopyWith(
+          PostulacionOfertaLaboralDTO value,
+          $Res Function(PostulacionOfertaLaboralDTO) then) =
+      _$PostulacionOfertaLaboralDTOCopyWithImpl<$Res>;
+  $Res call({String uuidEmpleado, String uuidEmpresa, String? comentario});
+}
+
+/// @nodoc
+class _$PostulacionOfertaLaboralDTOCopyWithImpl<$Res>
+    implements $PostulacionOfertaLaboralDTOCopyWith<$Res> {
+  _$PostulacionOfertaLaboralDTOCopyWithImpl(this._value, this._then);
+
+  final PostulacionOfertaLaboralDTO _value;
+  // ignore: unused_field
+  final $Res Function(PostulacionOfertaLaboralDTO) _then;
+
+  @override
+  $Res call({
+    Object? uuidEmpleado = freezed,
+    Object? uuidEmpresa = freezed,
+    Object? comentario = freezed,
+  }) {
+    return _then(_value.copyWith(
+      uuidEmpleado: uuidEmpleado == freezed
+          ? _value.uuidEmpleado
+          : uuidEmpleado // ignore: cast_nullable_to_non_nullable
+              as String,
+      uuidEmpresa: uuidEmpresa == freezed
+          ? _value.uuidEmpresa
+          : uuidEmpresa // ignore: cast_nullable_to_non_nullable
+              as String,
+      comentario: comentario == freezed
+          ? _value.comentario
+          : comentario // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$PostulacionOfertaLaboralDTOCopyWith<$Res>
+    implements $PostulacionOfertaLaboralDTOCopyWith<$Res> {
+  factory _$PostulacionOfertaLaboralDTOCopyWith(
+          _PostulacionOfertaLaboralDTO value,
+          $Res Function(_PostulacionOfertaLaboralDTO) then) =
+      __$PostulacionOfertaLaboralDTOCopyWithImpl<$Res>;
+  @override
+  $Res call({String uuidEmpleado, String uuidEmpresa, String? comentario});
+}
+
+/// @nodoc
+class __$PostulacionOfertaLaboralDTOCopyWithImpl<$Res>
+    extends _$PostulacionOfertaLaboralDTOCopyWithImpl<$Res>
+    implements _$PostulacionOfertaLaboralDTOCopyWith<$Res> {
+  __$PostulacionOfertaLaboralDTOCopyWithImpl(
+      _PostulacionOfertaLaboralDTO _value,
+      $Res Function(_PostulacionOfertaLaboralDTO) _then)
+      : super(_value, (v) => _then(v as _PostulacionOfertaLaboralDTO));
+
+  @override
+  _PostulacionOfertaLaboralDTO get _value =>
+      super._value as _PostulacionOfertaLaboralDTO;
+
+  @override
+  $Res call({
+    Object? uuidEmpleado = freezed,
+    Object? uuidEmpresa = freezed,
+    Object? comentario = freezed,
+  }) {
+    return _then(_PostulacionOfertaLaboralDTO(
+      uuidEmpleado: uuidEmpleado == freezed
+          ? _value.uuidEmpleado
+          : uuidEmpleado // ignore: cast_nullable_to_non_nullable
+              as String,
+      uuidEmpresa: uuidEmpresa == freezed
+          ? _value.uuidEmpresa
+          : uuidEmpresa // ignore: cast_nullable_to_non_nullable
+              as String,
+      comentario: comentario == freezed
+          ? _value.comentario
+          : comentario // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_PostulacionOfertaLaboralDTO implements _PostulacionOfertaLaboralDTO {
+  _$_PostulacionOfertaLaboralDTO(
+      {required this.uuidEmpleado, required this.uuidEmpresa, this.comentario});
+
+  factory _$_PostulacionOfertaLaboralDTO.fromJson(Map<String, dynamic> json) =>
+      _$_$_PostulacionOfertaLaboralDTOFromJson(json);
+
+  @override
+  final String uuidEmpleado;
+  @override
+  final String uuidEmpresa;
+  @override
+  final String? comentario;
+
+  @override
+  String toString() {
+    return 'PostulacionOfertaLaboralDTO(uuidEmpleado: $uuidEmpleado, uuidEmpresa: $uuidEmpresa, comentario: $comentario)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _PostulacionOfertaLaboralDTO &&
+            (identical(other.uuidEmpleado, uuidEmpleado) ||
+                const DeepCollectionEquality()
+                    .equals(other.uuidEmpleado, uuidEmpleado)) &&
+            (identical(other.uuidEmpresa, uuidEmpresa) ||
+                const DeepCollectionEquality()
+                    .equals(other.uuidEmpresa, uuidEmpresa)) &&
+            (identical(other.comentario, comentario) ||
+                const DeepCollectionEquality()
+                    .equals(other.comentario, comentario)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(uuidEmpleado) ^
+      const DeepCollectionEquality().hash(uuidEmpresa) ^
+      const DeepCollectionEquality().hash(comentario);
+
+  @JsonKey(ignore: true)
+  @override
+  _$PostulacionOfertaLaboralDTOCopyWith<_PostulacionOfertaLaboralDTO>
+      get copyWith => __$PostulacionOfertaLaboralDTOCopyWithImpl<
+          _PostulacionOfertaLaboralDTO>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$_$_PostulacionOfertaLaboralDTOToJson(this);
+  }
+}
+
+abstract class _PostulacionOfertaLaboralDTO
+    implements PostulacionOfertaLaboralDTO {
+  factory _PostulacionOfertaLaboralDTO(
+      {required String uuidEmpleado,
+      required String uuidEmpresa,
+      String? comentario}) = _$_PostulacionOfertaLaboralDTO;
+
+  factory _PostulacionOfertaLaboralDTO.fromJson(Map<String, dynamic> json) =
+      _$_PostulacionOfertaLaboralDTO.fromJson;
+
+  @override
+  String get uuidEmpleado => throw _privateConstructorUsedError;
+  @override
+  String get uuidEmpresa => throw _privateConstructorUsedError;
+  @override
+  String? get comentario => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$PostulacionOfertaLaboralDTOCopyWith<_PostulacionOfertaLaboralDTO>
+      get copyWith => throw _privateConstructorUsedError;
+}
